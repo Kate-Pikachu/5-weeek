24daef5522c79ce0cde68b2bbd91e11b54fcf1f9
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-13 16:25:02 +0300

    fix

--- task_3/main3.cpp
+++ task_3/main3.cpp
@@ -4,16 +4,16 @@
 
 int main(int argc, char** argv) {
 
-	std::string text = "kotik@mail.ru, octupus-3*3@swim.com, ca&py;bara@hate.you, dif#44^5gdf@list.ru";
+	std::string text = "kotik@mail.ru, octupus-3*3@swim.com, capybara@hate.you, dif44gdf@list.ru";
 	std::cout << "Email adress: " << text << std::endl;
 
-	std::regex pattern(R"((([0-9|A-Z|a-z]{1,}))@([A-Z|a-z]{1,}\.){1,2}[A-Z|a-z]{2,})");
+	std::regex pattern(R"((([\w][0-9A-z.]+[\w]))@([_[:alpha:]]+\.)+([_[:alpha:]]{2,6}))");
 
 	std::sregex_iterator begin(text.cbegin(), text.cend(), pattern);
 	std::sregex_iterator end;
 
 	std::for_each(begin, end, [](const std::smatch& m) {
-			std::cout << m[3] << std::endl;
+			std::cout << m[3] << m[4] << std::endl;
 		});
 
 	system("pause");
