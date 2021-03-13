21be8cf5d93495d1d401f1fc2a79df701634be50
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-13 16:43:22 +0300

    fix4

--- task_4/Proj4/main4.cpp
+++ task_4/Proj4/main4.cpp
@@ -5,9 +5,9 @@
 int main(int argc, char** argv) {
 
 
-	std::string data = "da%fj2021.03.11gj13:48:33ij";
+	std::string data = "dafj 2021.03.11 gj 325:1:21 ij 13:43:00  inhvv 2039.23.1" ;
 
-	std::regex time(R"(([0-2]*\d):(([0-5]*\d):([0-5]*\d)))");
+	std::regex time(R"((\d{2})(:[0-5]\d){2})");
 
 	std::sregex_iterator begin(data.cbegin(), data.cend(), time);
 	std::sregex_iterator endTime;
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
 		});
 
 
-	std::regex date(R"((([1-2]\d{3}).(([0]*[1-9])).([0-31]*\d)))");
+	std::regex date(R"((([1-2]\d{3}).(([1][0-2])|([0][1-9])).(([0-2]\d)|([3][0,1]))))");
 
 	std::sregex_iterator beginData(data.cbegin(), data.cend(), date);
 	std::sregex_iterator endDate;
